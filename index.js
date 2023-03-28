4f888aea4cca5101f2b9fc9489b4b651f6208776
sagedev2k16 <sagedev2k16@gmail.com>
2023-03-28 20:31:24 +0530

    CLI app - initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("Hello");
+
+import chalk from "chalk";
+import boxen from "boxen";
+import yargs from 'yargs';
+
+const options = yargs(process.argv.slice(2))
+    .usage("Usage: -n <name>")
+    .option("n", { alias: "name", describe: "Your name", type: "string", demandOption: true })
+    .argv;
+
+const greeting = chalk.yellowBright.bold.bgRed(`Hello, ${options.name}!`);
+
+const boxenOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "green",
+    backgroundColor: "#555555"
+};
+
+const msgBox = boxen(greeting, boxenOptions);
+
+console.log(msgBox);
